19f80f23445366d5fa3468f2a85077c91bc195f5
Federico Meini <federico.meini@gmail.com>
2022-03-09 22:57:57 +0100

    Fix Endpoint URL schema

--- config/runtime.exs
+++ config/runtime.exs
@@ -41,7 +41,7 @@ if config_env() == :prod do
       """
 
   config :chatty, ChattyWeb.Endpoint,
-    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", scheme: "https", port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
