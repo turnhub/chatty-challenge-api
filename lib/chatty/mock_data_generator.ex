4c595db2444fcf6ce5064a9136298d2b250fb527
Federico Meini <federico.meini@gmail.com>
2022-03-12 12:14:43 +0100

    Generate mock chats and messages

--- lib/chatty/mock_data_generator.ex
+++ lib/chatty/mock_data_generator.ex
@@ -3,8 +3,9 @@ defmodule Chatty.MockDataGenerator do
   alias Chatty.Accounts
   alias Chatty.Chats
   alias Chatty.Messages
-
-  @names ~w[Mark Alice Hugo Bob Anna]
+  alias Chatty.Mocks.Names
+  alias Chatty.Mocks.Numbers
+  alias Chatty.Mocks.Quotes
 
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, %{})
@@ -25,22 +26,77 @@ defmodule Chatty.MockDataGenerator do
   end
 
   defp schedule_work do
-    Process.send_after(self(), :do_work, 30_000)
+    Process.send_after(self(), :do_work, 15_000)
   end
 
   defp do_recurrent_thing(_state) do
     for user <- Accounts.list_users() do
-      from_name = @names |> Enum.random()
-
-      {:ok, chat} =
-        %{user_id: user.id, from_name: from_name, from_number: "+16543223456"}
-        |> Chats.create_chat()
-
-      for msg <- ~w[msg1 msg2 msg3 msg4 msg5] do
-        Messages.create_message(%{chat_id: chat.id, text: msg})
-      end
-
-      IO.puts("Created mock chat #{chat.id} for user #{user.email}")
+      generate_mock_data_for_user(user)
     end
+
+    :ok
+  end
+
+  defp generate_mock_data_for_user(user) do
+    chats = Chats.list_chats_for_user(user)
+    chats_count = Enum.count(chats)
+
+    generate_new_chat? = chats_count == 0 or :rand.uniform(100) < 20
+
+    if generate_new_chat? do
+      generate_mock_chat_for_user(user, chats)
+    else
+      generate_mock_mesage_for_chats(chats)
+    end
+
+    :ok
+  end
+
+  defp generate_mock_chat_for_user(user, existing_chats) do
+    from_name =
+      existing_chats
+      |> Enum.map(& &1.from_name)
+      |> Names.random_name()
+
+    from_number =
+      existing_chats
+      |> Enum.map(& &1.from_number)
+      |> Numbers.random_number()
+
+    {:ok, chat} =
+      Chats.create_chat(%{
+        user_id: user.id,
+        from_name: from_name,
+        from_number: from_number
+      })
+
+    for _ <- 1..:rand.uniform(5) do
+      Messages.create_message(%{
+        chat_id: chat.id,
+        text: Quotes.random_quote()
+      })
+    end
+
+    {:ok, chat}
+  end
+
+  defp generate_mock_mesage_for_chats(chats) do
+    chat_ids =
+      chats
+      |> Enum.sort_by(& &1.inserted_at, :asc)
+      |> Enum.map(& &1.id)
+
+    chat_ids_for_sorting =
+      chat_ids
+      |> Enum.reduce([], fn {id, index}, acc -> acc ++ List.duplicate(id, index + 1) end)
+
+    random_chat_id =
+      chat_ids_for_sorting
+      |> Enum.random()
+
+    Messages.create_message(%{
+      chat_id: random_chat_id,
+      text: Quotes.random_quote()
+    })
   end
 end
